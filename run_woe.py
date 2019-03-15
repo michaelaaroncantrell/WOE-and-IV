d1e7a55572340eab46ce9773ba28aef4df656722
Michael Cantrell <michaelaaroncantrell@gmail.com>
2019-03-15 07:22:29 -0500

    add train/test functionality

--- run_woe.py
+++ run_woe.py
@@ -1,35 +1,26 @@
-from sklearn.model_selection import StratifiedShuffleSplit
-from sklearn.model_selection import train_test_split
+from woe import get_woe_iv
 import numpy as np
 import pandas as pd
-from sklearn.metrics import roc_auc_score
-from sklearn.linear_model import LogisticRegression as LR
-from sklearn.feature_selection import RFE
-# from scipy import stats
-from scipy.stats import chi2
-from copy import deepcopy
-import random
-
-from woe import data_vars
 ##########################
 ### MAIN WOE FUNCTIONS ###
 ##########################
-def run_WOE(X_train, X_test, y_train, y_test, max_bin = 5):
+def WOE_train_test(X_train, X_test, y_train, y_test, max_cont_bin = 5):
     ''' 
-    Computes WOE on X_train, X_test. If keep_all, then WOE-transforms train and test and
-    returns. Otherwise, subsets variables on those with IV > 0.02, and looks at
-    variables with abs(correlation) > 0.5 and drops the variable with lower IV.
-    WOE-transforms remaining variables and returns.
+    Computes WOE on X_train and transofmrs X_train and X-test accordingly.
     '''
     
     #Get WOE
-    WOE, WOE_concise = data_vars(X_train, y_train, max_bin = max_bin) 
+    WOE, WOE_concise = get_woe_iv(X_train, y_train, max_cont_bin = max_cont_bin) 
+    WOE_no_gaps = close_bucket_gaps(WOE, X_train)
     
     #TRANSFORM BOTH TRAIN/TEST WITH THIS OUTPUT
-    X_WOE_tranformed_train = woe_transform_all_cols(X_WOE_subset_train, WOE)
-    X_WOE_tranformed_test = woe_transform_all_cols(X_WOE_subset_test, WOE)
-    
-    return(X_WOE_tranformed_train, X_WOE_tranformed_test, WOE, WOE_concise)
+    X_WOE_tranformed_train = woe_transform_all_cols(X_train, WOE_no_gaps)
+
+    #apply function here to change WOE so that that MIN and MAX of previous coincide.
+
+    X_WOE_tranformed_test = woe_transform_all_cols(X_test, WOE_no_gaps)
+
+    return(X_WOE_tranformed_train, X_WOE_tranformed_test, WOE, WOE_concise, WOE_no_gaps)
 
 
 
@@ -39,14 +30,110 @@ def run_WOE(X_train, X_test, y_train, y_test, max_bin = 5):
 
 
 def woe_transform_all_cols(X, WOE_df):
-    for col in list(X):
-        X = woe_transform_for_arb_col(col, X, WOE_df)
-    return(X)
+    X_new = X.copy() ### TODO: IS NECESSARY? ###
+    for col in X_new.columns:
+        curr_WOE = WOE_df[WOE_df.VAR_NAME == col]
+        X_new[col] = X_new[col].apply(lambda x: get_single_woe_value(x, curr_WOE, col)).fillna(0)
+    
+    return(X_new)
 
 
-def woe_transform_for_arb_col(col, X, WOE_df):
-    curr_WOE = WOE_df[WOE_df.VAR_NAME == col]
-    def tmp_fun(x, curr_WOE):
+def get_single_woe_value(x, curr_WOE, col):
+            
+    #check for first occurence of True
+    boo_arr = (x <= curr_WOE.MAX_VALUE)
+    #look for least idx where TRUE. IF none, then use last idx.
+    true_idx = [i for i,x in enumerate(boo_arr) if x]
+    if len(true_idx) != 0:
+        WOE_bin_idx = min(true_idx)
+        # print("first case")
+        return(curr_WOE.iloc[WOE_bin_idx].WOE)
+    else:
+        # print('second case')
+        # print(boo_arr)
+        WOE_bin_idx = boo_arr.index.max()
+        # print(boo_arr.index.max())
+        # print('curr_WOE', curr_WOE)
+        # print('WOE_bin_idx', WOE_bin_idx)
+        return(curr_WOE.loc[WOE_bin_idx].WOE)
+        # len(boo_arr)
+    # try: 
+    #     WOE_bin_idx = min(min(true_idx), len(boo_arr))
+    # except:
+    #     print('curr_WOE', curr_WOE)
+    #     print('col', col)
+    #     print('true_idx', true_idx)
+    #     print('x', x)
+    # print(boo_arr)
+    # print('true_idx', true_idx)
+    # print('WOE_bin_idx', WOE_bin_idx)
+    # print('x', x)
+    # print('curr_WOE', curr_WOE)
+    # print('curr_WOE.iloc[WOE_bin_idx].WOE', curr_WOE.iloc[WOE_bin_idx].WOE)
+
+
+
+
+
+
+
+    # #### old version ###
+    # #find the index in the WOE table that the value-to-be-transformed lies in            
+    # boo_arr = (curr_WOE.MIN_VALUE <= x) & (x <= curr_WOE.MAX_VALUE)
+
+
+    # #this is to handle edge case when max value of test is greater than that of train
+    # if not any(boo_arr): 
+    #     if x < curr_WOE.iloc[0].MIN_VALUE:
+    #         WOE_bin_idx = 0
+    #     elif x > curr_WOE.iloc[len(curr_WOE) - 1].MAX_VALUE:
+    #         WOE_bin_idx = len(curr_WOE)
+    #     # else:
+    #     #     print(col, x)
+    # if len([i for i, x in enumerate(boo_arr) if x]) == 0:
+    #     print('assigning value to 0', x, col)
+    #     return(0) #a hack because x may be inbetween row 1 max and row 2 min...
+    #     #unfortunately nans aren't handled well and are getting mapped to 0 too
+        
+    # WOE_bin_idx = [i for i, x in enumerate(boo_arr) if x][0]
+    # return(curr_WOE.iloc[WOE_bin_idx].WOE)
+
+
+
+def close_bucket_gaps(WOE_df, X): #assumed that WOE was calculated on X.
+    WOE_df_cols = WOE_df.columns
+    for col in X.columns:
+        if col in WOE_df.VAR_NAME.unique(): #should be, but for safety for users
+            if np.issubdtype(X[col].dtype, np.number): #only numeric cols
+                #for the current col, loop through WOE and close gap by bringing to middle value
+                sub_WOE_df = WOE_df[WOE_df.VAR_NAME == col]
+                #sort so missing value is last
+                orig_ixs = sub_WOE_df.index
+                sub_WOE_df.sort_values('MIN_VALUE', inplace = True)
+                sub_WOE_df.index = orig_ixs
+                for idx in sub_WOE_df[:-1].index:
+                    curr_row_max = sub_WOE_df[sub_WOE_df.index == idx].MAX_VALUE.iloc[0] #TODO MAKE THIS PRETTIER
+                    next_row_min = sub_WOE_df[sub_WOE_df.index == (idx + 1)].MIN_VALUE.iloc[0]
+                    if curr_row_max < next_row_min:
+                        midpoint = (next_row_min + curr_row_max) / 2
+                        #update sub_WOE_df
+                        sub_WOE_df.loc[sub_WOE_df.index == (idx + 1), 'MIN_VALUE'] = midpoint
+                        sub_WOE_df.loc[sub_WOE_df.index == idx, 'MAX_VALUE'] = midpoint
+                        # sub_WOE_df.iloc[(idx + 1)]['MIN_VALUE'] = midpoint
+                        # sub_WOE_df.iloc[idx]['MAX_VALUE'] = midpoint
+                #over-write WOE_df with sub_WOE_df
+                WOE_df = WOE_df[WOE_df.VAR_NAME != col]
+                WOE_df = pd.concat([WOE_df, sub_WOE_df])
+    #TODO sort the rows by original order of VAR_NAMES so user isn't confused, unless this output is just under hood
+    
+    #sort columns
+    WOE_df = WOE_df[WOE_df_cols]
+    return(WOE_df)
+    
+
+
+
+######################################################
 #         if X[col].dtype == 'object':
 #             print(col)
 #             display(curr_WOE)
@@ -57,26 +144,19 @@ def woe_transform_for_arb_col(col, X, WOE_df):
 #                 curr_WOE_null = curr_WOE[curr_WOE.MIN_VALUE.isna()]
 # #             print(curr_WOE_null[curr_WOE_null.COUNT == curr_WOE_null.COUNT.max()].WOE)
 #                 return(float(curr_WOE_null[curr_WOE_null.COUNT == curr_WOE_null.COUNT.max()].WOE))
-            
-        boo_arr = (curr_WOE.MIN_VALUE <= x) & (x <= curr_WOE.MAX_VALUE)
-        #this is to handle edge case when max value of test is greater than that of train
-        if not any(boo_arr): 
-            if x < curr_WOE.iloc[0].MIN_VALUE:
-                WOE_bin_idx = 0
-            elif x > curr_WOE.iloc[len(curr_WOE) - 1].MAX_VALUE:
-                WOE_bin_idx = len(curr_WOE)
-#             else:
-#                 print(col, x)
-        if len([i for i, x in enumerate(boo_arr) if x]) == 0:
-#             print('assigning value to 0', x, col)
-            return(0) #a hack because x may be inbetween row 1 max and row 2 min...
-                #unfortunately nans aren't handled well and are getting mapped to 0 too
-                #could fix this with more time. doesn't seem to be a big issue
 
-        WOE_bin_idx = [i for i, x in enumerate(boo_arr) if x][0]
-        return(curr_WOE.iloc[WOE_bin_idx].WOE)
 
-    X_new = X.copy()
-    # WARNING !!! FILLING NAS BECAUSE OF SHITTY NA HANDLING IN WOE SOFTWARE #
-    X_new[col] = X_new[col].apply(lambda x: tmp_fun(x, curr_WOE)).fillna(0)
-    return(X_new)
+
+
+
+
+
+
+
+# def woe_transform_for_arb_col(col, X, WOE_df):
+#     curr_WOE = WOE_df[WOE_df.VAR_NAME == col]
+
+#     X_new = X.copy()
+#     # WARNING !!! FILLING NAS BECAUSE OF SHITTY NA HANDLING IN WOE SOFTWARE #
+    
+#     return(X_new)
--- woe.py
+++ woe.py
@@ -1,4 +1,3 @@
-# import packages
 import pandas as pd
 import numpy as np
 import pandas.core.algorithms as algos
@@ -9,33 +8,29 @@ import traceback
 import string
 
 
-force_bin = 5
-
-# define a binning function
-def mono_bin(Y, X, n):
-    df1 = pd.DataFrame({"X": X, "Y": Y})
-    justmiss = df1[['X', 'Y']][df1.X.isnull()]
-    notmiss = df1[['X', 'Y']][df1.X.notnull()]
+def bin_conts_var(Y, X, max_cont_bin = 20, force_cont_bin = 5):
+    df = pd.DataFrame({"X": X, "Y": Y})
+    justmiss = df[['X', 'Y']][df.X.isnull()]
+    notmiss = df[['X', 'Y']][df.X.notnull()]
     r = 0
     while np.abs(r) < 1:
         try:
-            d1 = pd.DataFrame({"X": notmiss.X, "Y": notmiss.Y, "Bucket": pd.qcut(notmiss.X, n)})
+            d1 = pd.DataFrame({"X": notmiss.X, "Y": notmiss.Y, "Bucket": pd.qcut(notmiss.X, max_cont_bin)})
             d2 = d1.groupby('Bucket', as_index=True)
             r, p = stats.spearmanr(d2.mean().X, d2.mean().Y)
-            n = n - 1
+            max_cont_bin = max_cont_bin - 1
         except Exception as e:
-            n = n - 1
+            max_cont_bin = max_cont_bin - 1
 
     if len(d2) == 1:
-        n = force_bin
-        bins = algos.quantile(notmiss.X, np.linspace(0, 1, n))
+        bins = algos.quantile(notmiss.X, np.linspace(0, 1, force_cont_bin))
         if len(np.unique(bins)) == 2:
             bins = np.insert(bins, 0, 1)
             bins[1] = bins[1] - (bins[1] / 2)
         d1 = pd.DataFrame(
             {"X": notmiss.X, "Y": notmiss.Y, "Bucket": pd.cut(notmiss.X, np.unique(bins), include_lowest=True)})
         d2 = d1.groupby('Bucket', as_index=True)
-
+    
     d3 = pd.DataFrame({}, index=[])
     d3["MIN_VALUE"] = d2.min().X
     d3["MAX_VALUE"] = d2.max().X
@@ -50,7 +45,8 @@ def mono_bin(Y, X, n):
         d4["COUNT"] = justmiss.count().Y
         d4["EVENT"] = justmiss.sum().Y
         d4["NONEVENT"] = justmiss.count().Y - justmiss.sum().Y
-        d3 = d3.append(d4, ignore_index=True)
+        # d3 = d3.append(d4, ignore_index=True)
+        d3 = pd.concat([d3, d4])
 
     d3["EVENT_RATE"] = d3.EVENT / d3.COUNT
     d3["NON_EVENT_RATE"] = d3.NONEVENT / d3.COUNT
@@ -67,10 +63,10 @@ def mono_bin(Y, X, n):
     return (d3)
 
 
-def char_bin(Y, X):
-    df1 = pd.DataFrame({"X": X, "Y": Y})
-    justmiss = df1[['X', 'Y']][df1.X.isnull()]
-    notmiss = df1[['X', 'Y']][df1.X.notnull()]
+def bin_categ_var(Y, X):
+    df = pd.DataFrame({"X": X, "Y": Y})
+    justmiss = df[['X', 'Y']][df.X.isnull()]
+    notmiss = df[['X', 'Y']][df.X.notnull()]
     df2 = notmiss.groupby('X', as_index=True)
 
     d3 = pd.DataFrame({}, index=[])
@@ -95,8 +91,19 @@ def char_bin(Y, X):
     d3["WOE"] = np.log(d3.DIST_EVENT / d3.DIST_NON_EVENT)
     d3["IV"] = (d3.DIST_EVENT - d3.DIST_NON_EVENT) * np.log(d3.DIST_EVENT / d3.DIST_NON_EVENT)
     d3["VAR_NAME"] = "VAR"
-    d3 = d3[['VAR_NAME', 'MIN_VALUE', 'MAX_VALUE', 'COUNT', 'EVENT', 'EVENT_RATE', 'NONEVENT', 'NON_EVENT_RATE',
-             'DIST_EVENT', 'DIST_NON_EVENT', 'WOE', 'IV']]
+    d3 = d3[['VAR_NAME'
+           , 'MIN_VALUE'
+           , 'MAX_VALUE'
+           , 'COUNT'
+           , 'EVENT'
+           , 'EVENT_RATE'
+           , 'NONEVENT'
+           , 'NON_EVENT_RATE'
+           , 'DIST_EVENT'
+           , 'DIST_NON_EVENT'
+           , 'WOE'
+           , 'IV'
+           ]]
     d3 = d3.replace([np.inf, -np.inf], 0)
     d3.IV = d3.IV.sum()
     d3 = d3.reset_index(drop=True)
@@ -104,23 +111,27 @@ def char_bin(Y, X):
     return (d3)
 
 
-def data_vars(df1, target, max_bin = 3):
+def get_woe_iv(df, target, max_cont_bin = 5, force_cont_bin = 5):
     stack = traceback.extract_stack()
     filename, lineno, function_name, code = stack[-2]
     vars_name = re.compile(r'\((.*?)\).*$').search(code).groups()[0]
     final = (re.findall(r"[\w']+", vars_name))[-1]
 
-    x = df1.dtypes.index
+    x = df.dtypes.index
     count = -1
 
     for i in x:
         if i.upper() not in (final.upper()):
-            if np.issubdtype(df1[i], np.number) and len(Series.unique(df1[i])) > 2:
-                conv = mono_bin(target, df1[i], n = max_bin)
+            if np.issubdtype(df[i], np.number) and len(Series.unique(df[i])) > 2:
+                conv = bin_conts_var(Y = target
+                                    , X = df[i]
+                                    , max_cont_bin = max_cont_bin
+                                    , force_cont_bin = force_cont_bin
+                                    )
                 conv["VAR_NAME"] = i
                 count = count + 1
             else:
-                conv = char_bin(target, df1[i])
+                conv = bin_categ_var(target, df[i])
                 conv["VAR_NAME"] = i
                 count = count + 1
 
